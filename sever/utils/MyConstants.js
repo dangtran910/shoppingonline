67584500bb654c4ea06ca5816f662ce111941d57
dangtran910 <polamsim200@gmail.com>
2023-11-23 19:26:56 +0700

    init project

---
+++ sever/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'dang.kxlkliw.mongodb.net',
+    DB_USER: 'dangtran',
+    DB_PASS: 'RxOlyd1uCXJihwCy',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'adudeptrai',
+    JWT_EXPIRES: '3600000', // in milliseconds
+    EMAIL_USER: 'scsthejoker@gmail.com', // gmail service
+    EMAIL_PASS: 'sfrf qjbr awwc vers'
+  };
+  module.exports = MyConstants;
